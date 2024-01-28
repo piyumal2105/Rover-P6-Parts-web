f841b37da8c5bf419c632b5b339605e4318d7b95
IT21057892 <99260819+IT21057892@users.noreply.github.com>
2024-01-28 10:00:45 +0530

    start to implement contact us page

--- frontend/src/components/ContactUs/ContactUs.jsx
+++ frontend/src/components/ContactUs/ContactUs.jsx
@@ -1,7 +1,45 @@
 import React from "react";
+import { Card } from "react-bootstrap";
 
 function ContactUs() {
-  return <div>ContactUs</div>;
+  return (
+    <>
+      <Card
+        style={{
+          width: "100%",
+          maxHeight: "auto",
+          backgroundColor: "#262626",
+          borderRadius: "0px",
+        }}
+      >
+        <Card.Body>
+          <center>
+            <Card
+              style={{
+                borderColor: "#FF9900",
+                borderWidth: "3px",
+                backgroundColor: "#262626",
+              }}
+            >
+              <Card.Body>
+                <Card.Title
+                  style={{
+                    textAlign: "justify",
+                    fontFamily: "Montserrat, sans-serif",
+                    fontSize: "20px",
+                    fontWeight: 600,
+                    color: "white",
+                  }}
+                >
+                  <center>Contact Now</center>
+                </Card.Title>
+              </Card.Body>
+            </Card>
+          </center>
+        </Card.Body>
+      </Card>
+    </>
+  );
 }
 
 export default ContactUs;
