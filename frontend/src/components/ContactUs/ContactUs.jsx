541fa29e7518d171def09a55e72584d9d7bf6d2d
IT21057892 <99260819+IT21057892@users.noreply.github.com>
2024-01-29 21:13:05 +0530

    finish contact us page

--- frontend/src/components/ContactUs/ContactUs.jsx
+++ frontend/src/components/ContactUs/ContactUs.jsx
@@ -3,6 +3,8 @@ import Card from "react-bootstrap/Card";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 function ContactUs() {
   return (
@@ -39,14 +41,35 @@ function ContactUs() {
                   <center>Contact Now</center>
                   <br />
                   <br />
-                  <center>
-                    <Container>
-                      <Row>
-                        <Col>Form</Col>
-                        <Col>Map</Col>
-                      </Row>
-                    </Container>
-                  </center>
+                  <Container>
+                    <Row>
+                      <Col>
+                        <Form.Label htmlFor="fullname">Full Name</Form.Label>
+                        <Form.Control type="text" id="fullname" /> <br />
+                        <Form.Label htmlFor="fullname">Email</Form.Label>
+                        <Form.Control type="email" id="email" /> <br />
+                        <Form.Label htmlFor="fullname">Message</Form.Label>
+                        <Form.Control type="message" id="message" />
+                        <br />
+                        <center>
+                          <Button
+                            href="#sendmessage"
+                            style={{
+                              backgroundColor: "#FF9900",
+                              border: "1px solid #FF9900",
+                              color: "black",
+                              fontWeight: 600,
+                              fontFamily: "Montserrat, sans-serif",
+                              width: "auto",
+                            }}
+                          >
+                            Send Message
+                          </Button>
+                        </center>
+                      </Col>
+                      <Col>Google Map</Col>
+                    </Row>
+                  </Container>
                 </Card.Title>
               </Card.Body>
             </Card>
